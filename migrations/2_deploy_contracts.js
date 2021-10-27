007b474c36e1e6de012ca842d748fd5cb314b5f0
derekzx <chinzhixian@gmail.com>
2021-10-26 18:33:52 -0700

    add stop loss router

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 var LimitOrder = artifacts.require("UtopiaLimitOrderRouter");
+var StopLoss = artifacts.require("UtopiaStopLossRouter")
 
 module.exports = async function(deployer) {
-    await deployer.deploy(LimitOrder);
-    let limitOrder = await LimitOrder.deployed()
+    // await deployer.deploy(LimitOrder);
+    // let limitOrder = await LimitOrder.deployed()
 
-    console.log(limitOrder.address);
+    // console.log(limitOrder.address);
+
+    await deployer.deploy(StopLoss);
+    let stopLoss = await StopLoss.deployed()
+
+    console.log(stopLoss.address);
 };
